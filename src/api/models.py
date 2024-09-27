bef6d32b13d4907c0cc8bd937c158eaabf5858a5
Javier Ramos <162994894+JavierRamos1988@users.noreply.github.com>
2024-09-27 11:33:36 +0000

    primera impresion

--- src/api/models.py
+++ src/api/models.py
@@ -2,18 +2,97 @@ from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
 
-class User(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    email = db.Column(db.String(120), unique=True, nullable=False)
-    password = db.Column(db.String(80), unique=False, nullable=False)
-    is_active = db.Column(db.Boolean(), unique=False, nullable=False)
+# class User(db.Model):
+#     id = db.Column(db.Integer, primary_key=True)
+#     email = db.Column(db.db.String(120), unique=True, nullable=False)
+#     password = db.Column(db.db.String(80), unique=False, nullable=False)
+#     is_active = db.Column(db.Boolean(), unique=False, nullable=False)
 
-    def __repr__(self):
-        return f'<User {self.email}>'
+#     def __repr__(self):
+#         return f'<User {self.email}>'
+
+#     def serialize(self):
+#         return {
+#             "id": self.id,
+#             "email": self.email,
+#         }
+
+class User(db.Model):
+    __tablename__ = "users"
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(64),unique=True,nullable=False)
+    password = db.Column(db.db.String(80), unique=False, nullable=False)
+    is_active = db.Column(db.Boolean(), unique=False, nullable=False)
+    favorites = db.relationship("Favorite",back_populates="user",uselist=True)
+
+    def __repr__(self) -> str:
+        return f"<User {self.username}>"
 
     def serialize(self):
         return {
             "id": self.id,
-            "email": self.email,
-            # do not serialize the password, its a security breach
+            "username": self.username,
+            "is_active": self.is_active,
+        }
+
+class Favorite(db.Model):
+    __tablename__ = "favorites"
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer,db.ForeignKey('users.id'))
+    user = db.relationship("User",back_populates="favorites",uselist=False)
+    favorite_type = db.Column(db.String(20))  # 'planet' or 'people'
+    favorite_id = db.Column(db.Integer)
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "user_id": self.user_id,
+            "favorite_type": self.favorite_type,
+            "favorite_id": self.favorite_id
+        }
+    
+class People(db.Model):
+    __tablename__ = "people"
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(120),  nullable=False)
+    height = db.Column(db.String(120),  nullable=False)
+    mass = db.Column(db.String(120),  nullable=False)
+    hair_color = db.Column(db.String(120),  nullable=False)
+    skin_color = db.Column(db.String(120),  nullable=False)
+    eye_color = db.Column(db.String(120),  nullable=False)
+    birth_year = db.Column(db.String(120),  nullable=False)
+    gender = db.Column(db.String(120),  nullable=False)
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            "height": self.height,
+            "mass": self.mass,
+            "hair_color": self.hair_color,
+            "skin_color": self.skin_color,
+            "eye_color": self.eye_color,
+            "birth_year": self.birth_year,
+            "gender": self.gender
+        }
+
+class Planet(db.Model):
+    __tablename__ = "planets"
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(256), nullable=False)
+    diameter = db.Column(db.String(120),  nullable=False)
+    climate = db.Column(db.String(120),  nullable=False)
+    gravity = db.Column(db.String(120),  nullable=False)
+    terrain = db.Column(db.String(120),  nullable=False)
+    population = db.Column(db.String(120),  nullable=False)
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            "diameter": self.diameter,
+            "climate": self.climate,
+            "gravity": self.gravity,
+            "terrain": self.terrain,
+            "population": self.population
         }
--- src/api/routes.py
+++ src/api/routes.py
@@ -2,7 +2,7 @@
 This module takes care of starting the API Server, Loading the DB and Adding the endpoints
 """
 from flask import Flask, request, jsonify, url_for, Blueprint
-from api.models import db, User
+from api.models import db, User, People, Planets, Favorites
 from api.utils import generate_sitemap, APIException
 from flask_cors import CORS
 
@@ -23,4 +23,5 @@ def handle_hello():
 
 @api.route('/all_users', methods=['GET'])
 def get_all_users():
-    return jsonify({'msg':'OK'})
+    users=User.query.all()
+    return jsonify({'msg':'OK'}), 200
